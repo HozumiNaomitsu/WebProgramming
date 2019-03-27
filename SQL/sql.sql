baf26b42a1160108379238957e0cb5f7c344e5b7
HozumiNaomitsu <HozumiNaomitsu@users.noreply.github.com>
2019-03-27 16:56:05 +0900

    SQL

---
+++ SQL/sql.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE hozumi DEFAULT CHARACTER SET utf8;
+
+
+CREATE TABLE user
+ (
+     id SERIAL PRIMARY KEY,
+	 login_id varchar(255) UNIQUE NOT NULL ,
+     name varchar(255) NOT NULL,
+	 birth_date DATE NOT NULL,
+     password varchar(255)NOT NULL,
+	 create_date DATETIME NOT NULL, 
+     update_date DATETIME NOT NULL
+	 );
+ 
+ INSERT INTO user
+ (
+     id,
+	 login_id,
+	 name,
+     birth_date,
+	 password,
+	 create_date,
+	 update_date
+	 )
+	 VALUES
+	 (
+	 1,
+	 'admin',
+	 'ä«óùé“',
+	 '1000-10-10',
+	 'naopyuta',
+	  now(),
+	  now()
+	 );
